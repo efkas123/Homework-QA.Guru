69166e4fe044995bce7dd2b2f038e586ffa831d3
Filipp Kotov <contact@filippkotov.ru>
2025-08-07 11:31:33 +0300

    Set up a foundation for conflict

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -2,7 +2,7 @@ package guru.qa;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Main { "I am not sure I know what I'm doing rn"
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
